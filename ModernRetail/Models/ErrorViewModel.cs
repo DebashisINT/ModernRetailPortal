c2a40db9bee2e502a33166c70d453c464b919d32
PritiINT <62432969+PritiINT@users.noreply.github.com>
2024-12-02 15:53:57 +0530

    check in

--- ModernRetail/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ModernRetail.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
