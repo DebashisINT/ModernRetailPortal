4af9545c54f1935fbf42cb4a477f970782cf4b13
PritiINT <62432969+PritiINT@users.noreply.github.com>
2024-12-21 15:27:23 +0530

    check in

--- ModernRetail/Controllers/SecurityRolesController.cs
+++ ModernRetail/Controllers/SecurityRolesController.cs
@@ -44,6 +44,23 @@ namespace ModernRetail.Controllers
             TempData.Keep();
             return View();
         }
+
+
+        public ActionResult GroupMember()
+        {
+            EntityLayer.CommonELS.UserRightsForPage rights = BusinessLogicLayer.CommonBLS.CommonBL.GetUserRightSession("/SecurityRolesList", "SecurityRoles");
+            ViewBag.CanAdd = rights.CanAdd;
+            ViewBag.CanView = rights.CanView;
+            ViewBag.CanExport = rights.CanExport;
+            ViewBag.CanEdit = rights.CanEdit;
+            ViewBag.CanDelete = rights.CanDelete;
+            //objdata.grp_id = 0;
+            //TempData["grp_id"] = null;
+
+            //TempData.Keep();
+            return View();
+        }
+        
         public ActionResult Index()
         {
             if (TempData["grp_id"] != null)
@@ -55,9 +72,7 @@ namespace ModernRetail.Controllers
 
             }
             return View(objdata);
-        }
-
-        
+        }       
         private string GetSetGroupAccessValues(int GroupId)
         {
             string UserGroupRightsString = "";
@@ -750,7 +765,6 @@ namespace ModernRetail.Controllers
         {
             DataSet output = new DataSet();
             output = objdata.Edit(id);
-
             if (output.Tables[0].Rows.Count > 0)
             {
                 return Json(new
@@ -764,7 +778,6 @@ namespace ModernRetail.Controllers
                 return Json(new { name = "" }, JsonRequestBehavior.AllowGet);
             }
         }
-
         public ActionResult ExportList(int type)
         {
             switch (type)
@@ -851,5 +864,133 @@ namespace ModernRetail.Controllers
 
             return settings;
         }
+
+        public ActionResult PartialGroupMemberGridList(SecurityRolesModel model)
+        {
+            try
+            {
+                EntityLayer.CommonELS.UserRightsForPage rights = BusinessLogicLayer.CommonBLS.CommonBL.GetUserRightSession("/SecurityRolesList", "SecurityRoles");
+                ViewBag.CanAdd = rights.CanAdd;
+                ViewBag.CanView = rights.CanView;
+                ViewBag.CanExport = rights.CanExport;
+                ViewBag.CanEdit = rights.CanEdit;
+                ViewBag.CanDelete = rights.CanDelete;
+
+                string Is_GMPageLoad = string.Empty;
+                DataTable dt = new DataTable();
+
+                if (model.Is_GMPageLoad != "1")
+                    Is_GMPageLoad = "Ispageload";
+
+                ViewData["ModelData"] = model;
+                string Userid = Convert.ToString(Session["MRuserid"]);
+
+                String con = System.Configuration.ConfigurationSettings.AppSettings["DBConnectionDefault"];
+                SqlCommand sqlcmd = new SqlCommand();
+                SqlConnection sqlcon = new SqlConnection(con);
+                sqlcon.Open();
+                sqlcmd = new SqlCommand("PRC_MR_SECURITYROLESDETAILS", sqlcon);
+                sqlcmd.Parameters.Add("@ACTION", "GETGROUPMEMBER");
+                sqlcmd.Parameters.Add("@USER_ID", Userid);
+                sqlcmd.Parameters.Add("@ISPAGELOAD", model.Is_GMPageLoad);
+                sqlcmd.Parameters.Add("@ID", model.grp_id);
+                sqlcmd.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter da = new SqlDataAdapter(sqlcmd);
+                da.Fill(dt);
+                sqlcon.Close();
+                return PartialView("_PartialGroupMember", GetGroupMemberDetailsList(Is_GMPageLoad));
+
+            }
+            catch
+            {
+                return RedirectToAction("Logout", "Login", new { Area = "" });
+            }
+        }
+        public IEnumerable GetGroupMemberDetailsList(string Is_PageLoad)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ERP_ConnectionString"].ConnectionString;
+            string Userid = Convert.ToString(Session["MRuserid"]);
+            if (Is_PageLoad != "Ispageload")
+            {
+                ModernRetailDataContext dc = new ModernRetailDataContext(connectionString);
+                var q = from d in dc.MR_GROUPMEMBERLISTs
+                        where d.USERID == Convert.ToInt32(Userid)
+                        orderby d.SEQ ascending
+                        select d;
+                return q;
+            }
+            else
+            {
+                ModernRetailDataContext dc = new ModernRetailDataContext(connectionString);
+                var q = from d in dc.MR_GROUPMEMBERLISTs
+                        where d.SEQ == 0
+                        select d;
+                return q;
+            }
+        }
+
+        public ActionResult ExporGMList(int type)
+        {
+            switch (type)
+            {
+                case 1:
+                    return GridViewExtension.ExportToXlsx(GetGMGridViewSettings(), GetGroupMemberDetailsList(""));               
+                default:
+                    break;
+            }
+            return null;
+        }
+
+        private GridViewSettings GetGMGridViewSettings()
+        {
+            var settings = new GridViewSettings();
+            settings.Name = "PartialGroupMemberGridList";
+            settings.SettingsExport.ExportedRowType = GridViewExportedRowType.All;
+            settings.SettingsExport.FileName = "GroupMember";
+
+            settings.Columns.Add(x =>
+            {
+                x.FieldName = "USER_NAME";
+                x.Caption = "Name";
+                x.VisibleIndex = 1;
+                x.ColumnType = MVCxGridViewColumnType.TextBox;
+                x.Width = System.Web.UI.WebControls.Unit.Percentage(25);
+            });
+            settings.Columns.Add(x =>
+            {
+                x.FieldName = "DESIGNATION";
+                x.Caption = "Designation";
+                x.VisibleIndex = 2;
+                x.ColumnType = MVCxGridViewColumnType.TextBox;
+                x.Width = System.Web.UI.WebControls.Unit.Percentage(25);
+            });
+            settings.Columns.Add(x =>
+            {
+                x.FieldName = "GRSTATUS";
+                x.Caption = "User Status";
+                x.VisibleIndex = 3;
+                x.ColumnType = MVCxGridViewColumnType.TextBox;
+                x.Width = System.Web.UI.WebControls.Unit.Percentage(25);
+            });
+            settings.Columns.Add(x =>
+            {
+                x.FieldName = "ONLINESTATUS";
+                x.Caption = "Online Status";
+                x.VisibleIndex = 4;
+                x.ColumnType = MVCxGridViewColumnType.TextBox;
+                x.Width = System.Web.UI.WebControls.Unit.Percentage(25);
+            });
+
+
+            settings.SettingsExport.PaperKind = System.Drawing.Printing.PaperKind.A4;
+            settings.SettingsExport.LeftMargin = 20;
+            settings.SettingsExport.RightMargin = 20;
+            settings.SettingsExport.TopMargin = 20;
+            settings.SettingsExport.BottomMargin = 20;
+
+            return settings;
+        }
+
+
     }
 }
