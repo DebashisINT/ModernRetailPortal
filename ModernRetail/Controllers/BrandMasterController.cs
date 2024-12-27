7c15334c36337c452074b90caf98a9e264026e53
PritiINT <62432969+PritiINT@users.noreply.github.com>
2024-12-27 11:41:49 +0530

    check in

---
+++ ModernRetail/Controllers/BrandMasterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModernRetail.Controllers
+{
+    public class BrandMasterController : Controller
+    {
+        // GET: BrandMaster
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
