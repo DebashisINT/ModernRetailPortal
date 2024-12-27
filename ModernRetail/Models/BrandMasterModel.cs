b79eb29adace2374a0ad18c2f4cf30a6a5aceb57
PritiINT <62432969+PritiINT@users.noreply.github.com>
2024-12-27 16:44:44 +0530

    check in

--- ModernRetail/Models/BrandMasterModel.cs
+++ ModernRetail/Models/BrandMasterModel.cs
@@ -1,11 +1,103 @@
-﻿using System;
+﻿using DataAccessLayer;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
+using EntityLayer.MailingSystem;
 
 namespace ModernRetail.Models
 {
     public class BrandMasterModel
     {
+        public string Is_PageLoad { get; set; }
+
+        public int SaveBrand(string name, string user, string ID = "0", string ContactNo="", string Email="")
+        {
+            ProcedureExecute proc;
+
+            try
+            {
+                DataTable dt = new DataTable();
+                String con = System.Configuration.ConfigurationSettings.AppSettings["DBConnectionDefault"];
+                SqlCommand sqlcmd = new SqlCommand();
+                SqlConnection sqlcon = new SqlConnection(con);
+                sqlcon.Open();
+                sqlcmd = new SqlCommand("PRC_MR_BRANDMASTER", sqlcon);
+                if (ID == "0")
+                    sqlcmd.Parameters.AddWithValue("@ACTION", "ADD");
+                else
+                    sqlcmd.Parameters.AddWithValue("@ACTION", "UPDATE");
+                sqlcmd.Parameters.AddWithValue("@BRANDNAME", name);
+                sqlcmd.Parameters.AddWithValue("@USER_ID", user);
+                sqlcmd.Parameters.AddWithValue("@ID", ID);
+                sqlcmd.Parameters.AddWithValue("@Brand_ContactNo", ContactNo);
+                sqlcmd.Parameters.AddWithValue("@Brand_Email", Email);
+                SqlParameter output = new SqlParameter("@ReturnValue", SqlDbType.Int);
+                output.Direction = ParameterDirection.Output;
+                sqlcmd.Parameters.Add(output);
+
+                sqlcmd.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter da = new SqlDataAdapter(sqlcmd);
+                da.Fill(dt);
+                sqlcon.Close();
+
+                Int32 ReturnValue = Convert.ToInt32(output.Value);
+
+                sqlcmd.Dispose();
+                sqlcmd.Dispose();
+                return ReturnValue;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                proc = null;
+            }
+        }
+
+        public int Delete(string Id)
+        {
+            int i;
+            int rtrnvalue = 0;
+            ProcedureExecute proc = new ProcedureExecute("PRC_MR_BRANDMASTER");
+            proc.AddNVarcharPara("@action", 50, "DELETE");
+            proc.AddNVarcharPara("@ID", 30, Id);
+            proc.AddVarcharPara("@ReturnValue", 200, "0", QueryParameterDirection.Output);
+            i = proc.RunActionQuery();
+            rtrnvalue = Convert.ToInt32(proc.GetParaValue("@ReturnValue"));
+            return rtrnvalue;
+        }
+
+        public DataTable EditBrand(string ID)
+        {
+            ProcedureExecute proc;
+            int rtrnvalue = 0;
+            DataTable dt = new DataTable();
+            try
+            {
+                using (proc = new ProcedureExecute("PRC_MR_BRANDMASTER"))
+                {
+                    proc.AddVarcharPara("@ID", 100, ID);
+                    proc.AddVarcharPara("@ACTION", 100, "EDIT");
+                    dt = proc.GetTable();
+                    return dt;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            finally
+            {
+                proc = null;
+            }
+        }
     }
 }
