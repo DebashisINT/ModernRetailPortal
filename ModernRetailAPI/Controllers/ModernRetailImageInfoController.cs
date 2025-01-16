d6e29edbda40345df6bda6a3e0c98a6469bfdb86
Debashis Talukder <debashis.talukder@intglobal.com>
2025-01-16 21:15:10 +0530

    Commit
    Done

--- ModernRetailAPI/Controllers/ModernRetailImageInfoController.cs
+++ ModernRetailAPI/Controllers/ModernRetailImageInfoController.cs
@@ -1,6 +1,6 @@
 ﻿#region======================================Revision History=========================================================
 //Written By : Debashis Talukder On 09/12/2024
-//Purpose: Modern Retail Info Details.Row: 5
+//Purpose: Modern Retail Info Details.Row: 5,17
 #endregion===================================End of Revision History==================================================
 
 using System;
@@ -48,6 +48,59 @@ namespace ModernRetailAPI.Controllers
                 sqlcmd.Parameters.AddWithValue("@USER_ID", hhhh.user_id);
                 sqlcmd.Parameters.AddWithValue("@STORE_ID", hhhh.store_id);
                 sqlcmd.Parameters.AddWithValue("@PathImage", "/CommonFolder/StoreImage/" + ImageName);
+
+                sqlcmd.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter da = new SqlDataAdapter(sqlcmd);
+                da.Fill(dt);
+                sqlcon.Close();
+                if (dt.Rows.Count > 0)
+                {
+                    omodel.status = "200";
+                    omodel.message = "Success.";
+                }
+                else
+                {
+                    omodel.status = "202";
+                    omodel.message = "Image Not Save.";
+                }
+            }
+            catch (Exception msg)
+            {
+                omodel.status = "204" + ImageName;
+                omodel.message = msg.Message;
+            }
+            return Json(omodel);
+        }
+        public JsonResult StockImageSave(StockImageSaveInput model)
+        {
+            StockImageSaveOutput omodel = new StockImageSaveOutput();
+            string ImageName = "";
+            StockImageSaveInputDetails omedl2 = new StockImageSaveInputDetails();
+            String APIHostingPort = System.Configuration.ConfigurationManager.AppSettings["APIHostingPort"];
+            string UploadFileDirectory = "~/CommonFolder";
+            try
+            {
+                var hhhh = Newtonsoft.Json.JsonConvert.DeserializeObject<StockImageSaveInputDetails>(model.data);
+
+                if (!string.IsNullOrEmpty(model.data))
+                {
+                    ImageName = model.attachments.FileName;
+                    string vPath = Path.Combine(Server.MapPath("~/CommonFolder/StockImage"), ImageName);
+                    model.attachments.SaveAs(vPath);
+                }
+
+                DataTable dt = new DataTable();
+                String con = System.Configuration.ConfigurationManager.AppSettings["DBConnectionDefault"];
+
+                SqlCommand sqlcmd = new SqlCommand();
+                SqlConnection sqlcon = new SqlConnection(con);
+                sqlcon.Open();
+
+                sqlcmd = new SqlCommand("PRC_MDRINFODETAILS", sqlcon);
+                sqlcmd.Parameters.AddWithValue("@ACTION", "STOCKIMAGESAVE");
+                sqlcmd.Parameters.AddWithValue("@USER_ID", hhhh.user_id);
+                sqlcmd.Parameters.AddWithValue("@STOCK_ID", hhhh.stock_id);
+                sqlcmd.Parameters.AddWithValue("@PathImage", "/CommonFolder/StockImage/" + ImageName);
 
                 sqlcmd.CommandType = CommandType.StoredProcedure;
                 SqlDataAdapter da = new SqlDataAdapter(sqlcmd);
