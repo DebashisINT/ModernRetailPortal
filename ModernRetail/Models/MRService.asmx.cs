dd16b0cf5f2cef5ead76e22b3eaa6f2b383f3cd2
PritiINT <62432969+PritiINT@users.noreply.github.com>
2025-01-18 00:01:24 +0530

    CHECK IN

--- ModernRetail/Models/MRService.asmx.cs
+++ ModernRetail/Models/MRService.asmx.cs
@@ -18,6 +18,12 @@ namespace ModernRetail.Models
     public class MRService : System.Web.Services.WebService
     {
 
+        string ConnectionString = String.Empty;
+        public MRService()
+        {
+            ConnectionString = Convert.ToString(System.Web.HttpContext.Current.Session["DBConnectionDefault"]);
+        }
+
         [WebMethod(EnableSession = true)]
         [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
         public object GetOnDemandReportTo(string SearchKey)
@@ -181,7 +187,124 @@ namespace ModernRetail.Models
             return listProduct;
         }
 
+        [WebMethod(EnableSession = true)]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public object GetTargetLevelDetailsList(string SearchKey, string Type)
+        {
 
+            List<SalesTargetLevelDetails> list = new List<SalesTargetLevelDetails>();
+
+            string USERID = Convert.ToString(Session["MRuserid"]);
+            SearchKey = SearchKey.Replace("'", "''");
+            BusinessLogicLayer.DBEngine oDBEngine = new BusinessLogicLayer.DBEngine(ConnectionString);
+            string Query = "";
+            Query = @"   EXEC PRC_MR_TARGETASSIGN @Action='" + Type + "',@SearchKey='" + SearchKey + "',@USERID='" + USERID + "' ";
+
+            DataTable dt = oDBEngine.GetDataTable(Query);
+            if (!String.IsNullOrEmpty(Type))
+            {
+                list = (from DataRow dr in dt.Rows
+                        select new SalesTargetLevelDetails()
+                        {
+                            Level_ID = Convert.ToString(dr["ID"]),
+                            Level_Name = Convert.ToString(dr["NAME"]),
+                            Level_Code = Convert.ToString(dr["CODE"]),
+
+
+                        }).ToList();
+            }
+            else
+            {
+                list = (from DataRow dr in dt.Rows
+                        select new SalesTargetLevelDetails()
+                        {
+                            Level_ID = Convert.ToString(dr["ID"]),
+                            Level_Name = Convert.ToString(dr["NAME"]),
+                            Level_Code = Convert.ToString(dr["CODE"]),
+
+
+                        }).ToList();
+            }
+
+            return list;
+        }
+
+        [WebMethod(EnableSession = true)]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public object GetTargetProductDetailsList(string SearchKey)
+        {
+
+            List<ProductDetails> list = new List<ProductDetails>();
+
+
+            SearchKey = SearchKey.Replace("'", "''");
+            BusinessLogicLayer.DBEngine oDBEngine = new BusinessLogicLayer.DBEngine(ConnectionString);
+            string Query = "";
+            Query = @"   EXEC PRC_MR_TARGETASSIGN @Action='GETPRODUCTLIST',@SearchKey='" + SearchKey + "' ";
+
+            DataTable dt = oDBEngine.GetDataTable(Query);
+            if (dt.Rows.Count > 0 && dt != null)
+            {
+                list = (from DataRow dr in dt.Rows
+                        select new ProductDetails()
+                        {
+                            ID = Convert.ToString(dr["ID"]),
+                            Name = Convert.ToString(dr["NAME"]),
+                            Code = Convert.ToString(dr["CODE"]),
+
+
+                        }).ToList();
+            }
+            else
+            {
+                list = (from DataRow dr in dt.Rows
+                        select new ProductDetails()
+                        {
+                            ID = Convert.ToString(dr["ID"]),
+                            Name = Convert.ToString(dr["NAME"]),
+                            Code = Convert.ToString(dr["CODE"]),
+
+
+                        }).ToList();
+            }
+
+            return list;
+        }
+
+        [WebMethod(EnableSession = true)]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public object GetTargetBrandDetailsList(string SearchKey)
+        {
+
+            List<BrandDetails> list = new List<BrandDetails>();
+            SearchKey = SearchKey.Replace("'", "''");
+            BusinessLogicLayer.DBEngine oDBEngine = new BusinessLogicLayer.DBEngine(ConnectionString);
+            string Query = "";
+            Query = @"   EXEC PRC_MR_TARGETASSIGN @Action='GETBRANDLIST',@SearchKey='" + SearchKey + "'";
+
+            DataTable dt = oDBEngine.GetDataTable(Query);
+            if (dt.Rows.Count > 0 && dt != null)
+            {
+                list = (from DataRow dr in dt.Rows
+                        select new BrandDetails()
+                        {
+                            Brand_ID = Convert.ToString(dr["ID"]),
+                            Brand_Name = Convert.ToString(dr["NAME"]),
+
+                        }).ToList();
+            }
+            else
+            {
+                list = (from DataRow dr in dt.Rows
+                        select new BrandDetails()
+                        {
+                            Brand_ID = Convert.ToString(dr["ID"]),
+                            Brand_Name = Convert.ToString(dr["NAME"]),
+                        }).ToList();
+            }
+
+            return list;
+        }
         public class UsersModel
         {
             public string USER_ID { get; set; }
@@ -215,6 +338,27 @@ namespace ModernRetail.Models
             public string sProducts_Code { get; set; }
             public string sProducts_Name { get; set; }
             //public string sProducts_Description { get; set; }
+        }
+
+        public class SalesTargetLevelDetails
+        {
+            public string Level_ID { get; set; }
+            public string Level_Name { get; set; }
+            public string Level_Code { get; set; }
+
+        }
+        public class ProductDetails
+        {
+            public string ID { get; set; }
+            public string Name { get; set; }
+            public string Code { get; set; }
+
+        }
+
+        public class BrandDetails
+        {
+            public string Brand_ID { get; set; }
+            public string Brand_Name { get; set; }
         }
 
     }
